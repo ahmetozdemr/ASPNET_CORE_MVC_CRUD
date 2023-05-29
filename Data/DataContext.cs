ba4fd4622688621a319d3539179f56395619ed41
Ahmet Özdemir <ahmetzdmiir@gmail.com>
2023-05-29 05:41:09 +0300

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using ASPNET_CORE_MVC_CRUD.Models.CustomerModel;
+using ASPNET_CORE_MVC_CRUD.Models.EmployeeModel;
+using ASPNET_CORE_MVC_CRUD.Models.ProductModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNET_CORE_MVC_CRUD.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //    optionsBuilder.UseSqlServer("Server=AHMET-PC;Database=MvcCoreDemoDb;Trusted_Connection=True;TrustServerCertificate =true;");
+        //}
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
